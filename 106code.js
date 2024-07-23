c2d4db9c5ff6f205c73bfcd90c4c42f526277e1e
qianxuntle <3132588119@qq.com>
2024-07-23 14:03:55 +0800

    Add files via upload

--- 106code.js
+++ 106code.js
@@ -13,3 +13,18 @@ function enableScroll() {
 function disableScroll() {
     document.getElementById('windows-wrapper').style.overflow = 'hidden';
 }
+function openPreview() {
+    // 在新标签页中打开当前目录下的preview.html文件
+    window.open('lovedaima.html', '_blank');
+}
+
+function downloadFile() {
+    // 触发浏览器下载指定的zip文件
+    const downloadUrl = '爱心代码.zip'; // 使用相对路径或绝对路径
+    const link = document.createElement('a');
+    link.href = downloadUrl;
+    link.download = 'filename.zip'; // 设置下载的文件名
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
