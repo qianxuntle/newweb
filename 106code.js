0b002c7f0d007a6103e3141d707d59f633c891b6
qianxuntle <3132588119@qq.com>
2024-07-23 13:06:05 +0800

    Add files via upload

--- 106code.js
+++ 106code.js
@@ -6,3 +6,10 @@ function scrollToElement(elementId) {
 function openNewPage(url) {
     window.location.href = url; // 直接在当前页面加载新页面
 }
+function enableScroll() {
+    document.getElementById('windows-wrapper').style.overflow = 'auto';
+}
+
+function disableScroll() {
+    document.getElementById('windows-wrapper').style.overflow = 'hidden';
+}
