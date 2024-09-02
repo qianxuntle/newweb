d3062bee6552184e90b5ba835caee46da12b3a47
qianxuntle <3132588119@qq.com>
2024-09-02 23:15:08 +0800

    Add files via upload

--- rice.js
+++ rice.js
@@ -1,38 +1,64 @@
-var randomname = document.querySelector("#yuanpanyangshi2");
-var name1 = ["豆花饭", "套餐饭", "卤肉饭", "炒饭", "称菜"];
-var names = [];
-var run = false;
-function init() {
-    for (var i = 0; i < name1.length; i++) {
-        names.push(name1[Math.random() * name1.length | 0]);
+var floors = {
+    '一食堂': ['炒饭', '套餐饭', '石锅饭', '一楼麻辣烫', '面', '卤肉饭', '二楼麻辣烫'],
+    '二食堂': ['豆花饭', '卤肉饭', '炒饭', '麻辣烫'],
+    '三食堂': ['未知'],
+    '四食堂': ['未知'],
+    '我全都要': ['一食堂豆花饭', '一食堂炒饭', '套餐饭', '石锅饭', '一楼麻辣烫', '面', '一食堂卤肉饭', '二楼麻辣烫', '二食堂豆花饭', '二食堂卤肉饭', '二食堂炒饭', '二食堂麻辣烫', '未知']
+};
+
+function updateDishes() {
+    var floorSelect = document.getElementById('floorSelect');
+    var dishesDiv = document.getElementById('dishesDiv');
+    var selectedFloor = floorSelect.value;
+    var dishes = floors[selectedFloor];
+
+    // 清空之前的菜品
+    dishesDiv.innerHTML = '';
+
+    // 生成菜品列表
+    dishes.forEach(function (dish) {
+        var checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.value = dish;
+        checkbox.id = dish;
+        var label = document.createElement('label');
+        label.htmlFor = dish;
+        label.appendChild(document.createTextNode(dish));
+        dishesDiv.appendChild(checkbox);
+        dishesDiv.appendChild(label);
+        // dishesDiv.appendChild(document.createElement('br'));
+    });
+}
+
+function selectRandomDish() {
+    var checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+    var selectedDishes = Array.from(checkboxes).map(function (checkbox) {
+        return checkbox.value;
+    });
+
+    if (selectedDishes.length === 0) {
+        alert('请至少选择一个菜品！');
+        return;
     }
-}
-init();
-for (var i in names) {
-    let span = document.createElement("span");
-    span.innerText = names[i];
-    randomname.appendChild(span);
-}
-var spans = randomname.children;
-var deg = 0;
-var slow = 0;
-function spinWheel() {
-    run =! run;
-}
-function draw() {
-    for (var i = 0; i < spans.length; i++) {
-        var span = spans[i];
-        span.style.transform = "rotate(" + (i / names.length * 360 + deg) + "deg)"
-        span.style.color = "black"
+
+    let intervalId; // 用于存储定时器的ID
+
+    function getRandomName() {
+        const randomIndex = Math.floor(Math.random() * selectedDishes.length); // 生成一个随机索引
+        return selectedDishes[randomIndex]; // 返回随机选中的名字
     }
-    var now = ((360 - deg) / 360 * names.length | 0) % names.length;
-    randomname.setAttribute('data', names[now]);
-    spans[now].style.color = "red";
-    if (run) {
-        slow = 6;
-    } else {
-        slow = slow * 0.995;
+
+    function printRandomName() {
+        const nameDiv = document.getElementById('selectedDishesDiv'); // 获取div元素
+        nameDiv.textContent = getRandomName(); // 设置div的文本内容为随机名字
     }
-    deg -= slow;
+
+    function startRandomDishTimer() {
+        printRandomName(); // 首先打印一个名字
+        intervalId = setInterval(printRandomName, 50); // 每0.05秒打印一个新的名字
+        setTimeout(() => {
+            clearInterval(intervalId); // 5秒后停止定时器
+        }, 5000);
+    }
+    startRandomDishTimer(); // 直接调用函数开始定时器
 }
-setInterval(draw, 10);
